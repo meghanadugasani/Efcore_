fa84d2539f325c4f1dffd0d5c04a267c975610a7
meghanadugasani <meghanadugasani@gmail.com>
2025-08-05 23:07:58 +0530

    Add files via upload

---
+++ PlantDBFirst_Console/Models/KaniniContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace plantdbfirst_console.Models;
+
+public partial class KaniniContext : DbContext
+{
+    public KaniniContext()
+    {
+    }
+
+    public KaniniContext(DbContextOptions<KaniniContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Plant> Plants { get; set; }
+
+  
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("data source=MEGHANA;database=mvcefcore;integrated security=true;trustservercertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Plant>(entity =>
+        {
+            entity.HasKey(e => e.PlantId).HasName("PK__Plants__98FE395CF230E75C");
+
+            entity.Property(e => e.PlantName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+        });
+    }
+
+      
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ PlantDBFirst_Console/Models/Plant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace plantdbfirst_console.Models;
+
+public partial class Plant
+{
+    public int PlantId { get; set; }
+
+    public string PlantName { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public int Quantity { get; set; }
+}
