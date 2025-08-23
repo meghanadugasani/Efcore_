1f7e6b7249d0bfbec2359394b49aa22a330b3180
meghanadugasani <meghanadugasani@gmail.com>
2025-08-23 22:04:50 +0530

    Update Program.cs

--- API_PROJECT/Program.cs
+++ API_PROJECT/Program.cs
@@ -1,1 +1,114 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using WardrobeAPI.Data;
+using WardrobeAPI.Interfaces;
+using WardrobeAPI.Repositories;
+using WardrobeAPI.Services;
+
+namespace WardrobeAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<WardrobeContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("WardrobeDB")));
+
+
+            builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+        options.SerializerSettings.ReferenceLoopHandling =
+            Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            builder.Services.AddScoped<IUser, UserRepository>();
+            builder.Services.AddScoped<IWardrobe, WardrobeRepository>();
+            builder.Services.AddScoped<IDress, DressRepository>();
+            builder.Services.AddScoped<IOutfit, OutfitRepository>();
+            builder.Services.AddScoped<IOutfitDress, OutfitDressRepository>();
+
+            builder.Services.AddScoped<UserService>();
+            builder.Services.AddScoped<WardrobeService>();
+            builder.Services.AddScoped<DressService>();
+            builder.Services.AddScoped<OutfitService>();
+            builder.Services.AddScoped<OutfitDressService>();
+            var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!);
+
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+
+            builder.Services.AddSwaggerGen(c => {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+ {
+ {
+ new OpenApiSecurityScheme
+ {
+ Reference = new OpenApiReference
+ {
+ Type = ReferenceType.SecurityScheme,
+ Id = "Bearer"
+ }
+ },
+ Array.Empty<string>()
+ }
+ });
+            });
+
+
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
 
